53e5ae30eba310caf7ff98cc744b1d6ad8c13808
Ititi Obidah <iobidah9@gmail.com>
2019-04-26 19:43:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(sentence)
-   reversed = sentence.split(" ").collect do |word|
-   word.reverse
+reversed_array = sentence.split(' ').collect do |word|
+  word.reverse
+ end
+ reversed_array.join(" ")
 end 
-reversed.join(" ")
-end 
+      
